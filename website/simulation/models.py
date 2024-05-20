c4f231104730e054ca9d9931dd542cc21db2815e
51moon <gh51moon@outlook.com>
2024-05-20 22:05:30 +0200

    update model

--- website/simulation/models.py
+++ website/simulation/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class SimulationData(models.Model):
-    name = models.CharField(max_length=100)
-    value = models.FloatField()
-    unit = models.CharField(max_length=20)
+    height = models.FloatField(default=1) # m
+    velocity = models.FloatField(default=20) # m/s
+    angle = models.FloatField(default=40) # °
+    mass = models.FloatField(default=1) # kg
+    cwArho = models.FloatField(default=0.5) # kg/m
+    xmax = models.FloatField(default=6) # m
+    ymax = models.FloatField(default=4) # m
