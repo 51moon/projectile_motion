ff58ecac4b81214cfbef3bd65983217bca057632
51moon <gh51moon@outlook.com>
2024-05-24 17:44:28 +0200

    remove defaults from simulation model

--- website/simulation/models.py
+++ website/simulation/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class SimulationData(models.Model):
-    height = models.FloatField(default=1) # m
-    velocity = models.FloatField(default=20) # m/s
-    angle = models.FloatField(default=40) # °
-    mass = models.FloatField(default=1) # kg
-    cwArho = models.FloatField(default=0.5) # kg/m
-    xmax = models.FloatField(default=6) # m
-    ymax = models.FloatField(default=4) # m
+    height = models.FloatField() # m
+    velocity = models.FloatField() # m/s
+    angle = models.FloatField() # °
+    mass = models.FloatField() # kg
+    cwArho = models.FloatField() # kg/m
+    xmax = models.FloatField() # m
+    ymax = models.FloatField() # m
